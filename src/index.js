d0650e5e9802af361fb17c63bc7231233809e013
KeerthiNarayana <keerthinarayanaprasad2009@gmail.com>
2022-10-13 08:12:45 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register(
+  "https://comfy-jalebi-4cab0e.netlify.app/serviceWorker.js"
+);
 
 // 'https://glittering-mermaid-0e25f4.netlify.app/service-worker.js'
