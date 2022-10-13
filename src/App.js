90382b7fa0b94edd04cce159c109cd513c6adcef
KeerthiNarayana <keerthinarayanaprasad2009@gmail.com>
2022-10-13 08:13:08 +0530

    commit4

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
                   Bundle version :{" "}
                   <code>
                     v{global.appVersion}
-                    {Math.random()}:abb
+                    {Math.random()}:abbjj
                   </code>
                 </p>
               </header>
