5df147b0de74667759440676fd31b2639f07c345
KeerthiNarayana <keerthinarayanaprasad2009@gmail.com>
2022-11-07 08:01:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import CacheBuster from "./CacheBuster";
 import "./App.css";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <CacheBuster>
-        {({ loading, isLatestVersion, refreshCacheAndReload }) => {
-          if (loading) return null;
-          if (!loading && !isLatestVersion) {
-            refreshCacheAndReload();
-          }
+const App = () => {
+  const [data, setData] = useState();
+  axios
+    .get("/data.json")
+    .then((res) => setData(res.data))
+    .catch((err) => console.log(err));
+  return (
+    <CacheBuster>
+      {({ loading, isLatestVersion, refreshCacheAndReload }) => {
+        if (loading) return null;
+        if (!loading && !isLatestVersion) {
+          refreshCacheAndReload();
+        }
 
-          return (
-            <div className="App">
-              <header className="App-header">
-                <h1>Cache Bustings </h1>
-                <p>
-                  Bundle version : <code>v{global.appVersion}abc</code>
-                </p>
-              </header>
-            </div>
-          );
-        }}
-      </CacheBuster>
-    );
-  }
-}
+        return (
+          <div className="App">
+            <header className="App-header">
+              <h1>Cache Bustings </h1>
+              <p>
+                Bundle version :{" "}
+                <code>
+                  v{global.appVersion}
+                  {data.name}
+                </code>
+              </p>
+            </header>
+          </div>
+        );
+      }}
+    </CacheBuster>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+// axios.get()
+
+//   render() {
+
+//   }
+// }
+
+// export default App;
